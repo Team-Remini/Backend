6f6595787409a3f31ff0154deb71f7fe2891f526
clap_0 <clap-0@naver.com>
2023-09-27 15:11:08 +0900

    Init project

---
+++ src/main/java/com/likelion/remini/ReminiApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.remini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/remini/ReminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.remini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
