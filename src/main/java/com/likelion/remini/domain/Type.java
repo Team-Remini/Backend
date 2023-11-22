0a4e64c1195a1c6d4fb6f72410d52e0cac086fe6
clap_0 <clap-0@naver.com>
2023-11-22 19:18:30 +0900

    feat: 구독 모델과 회고 카테고리 Enum 클래스에 name 필드 추가

--- src/main/java/com/likelion/remini/domain/Type.java
+++ src/main/java/com/likelion/remini/domain/Type.java
@@ -1,14 +1,22 @@
 package com.likelion.remini.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Type {
-    KPT,
-    CSS,
-    FIVE_F,
-    TIL,
-    FOUR_L,
-    ORID,
-    AAR,
-    YWT,
-    PERSONAL,
-    RESULT
+    KPT("KPT 회고"),
+    CSS("Continue-Stop-Start 회고"),
+    FIVE_F("5F 회고"),
+    TIL("TIL 회고"),
+    FOUR_L("4L 회고"),
+    ORID("ORID 회고"),
+    AAR("AAR 회고"),
+    YWT("YWT 회고"),
+    PERSONAL("개인적 회고"),
+    RESULT("성과/수치 중심 회고"),
+    ;
+
+    private final String name;
 }
